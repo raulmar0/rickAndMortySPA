eca2003d93efeef3451720d4aac72cfef55ab525
raulmar <raulhdz40@gmail.com>
2020-10-25 02:33:00 -0600

    Cuarto intendo de arreglar las rutas en gh pages

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-log">
                 <h1>
-                    <a href="rickAndMortySPA/">
+                    <a href="/">
                         100tifi.co
                     </a>
                 </h1>
